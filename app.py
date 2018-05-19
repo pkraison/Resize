d3d0879e8c8df8231b8d6d92d9eb8e8d876bc47a
PRASHANT KUMAR RAI <prashant81995@gmail.com>
2018-05-19 23:43:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,6 +164,6 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 if __name__ == '__main__':
-    #app.debug = True
-    port = int(os.environ.get("PORT", 5000))
+    app.debug = True
+    port = int(os.environ.get('PORT', 33507))
     app.run(host="0.0.0.0", port=port)
